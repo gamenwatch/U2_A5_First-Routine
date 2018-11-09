b36edf1628caed583b338e8b72c080de57a9a435
jalac9997 <jalac9997@7KYM6Y1.Student.UGDSB.ED>
2018-11-09 10:07:28 -0500

    -created

--- src/u2_a5_first/subroutine/U2_A5_FirstSubroutine.java
+++ src/u2_a5_first/subroutine/U2_A5_FirstSubroutine.java
@@ -24,12 +24,12 @@ public class U2_A5_FirstSubroutine {
         Scanner keyedInput = new Scanner(System.in);
         
         //mass
-        System.out.println("What is your mass?");
+        System.out.println("What is your mass? (kg)");
         double mass;
         mass = keyedInput.nextInt();
         
         //acceleration
-        System.out.println("What is your acceleration?");
+        System.out.println("What is your acceleration? (m/s^2)");
         double acceleration;
         acceleration = keyedInput.nextInt();
         
@@ -42,20 +42,39 @@ public class U2_A5_FirstSubroutine {
     public static void forceTwoOut() {
         Scanner keyedInput = new Scanner(System.in);
         
-        //mass
-        System.out.println("What is your mass?");
-        double mass;
-        mass = keyedInput.nextInt();
+        //force upwards
+        System.out.println("What is the magnitude of the force upward? (N)");
+        double forceUp;
+        forceUp = keyedInput.nextInt();
         
-        //acceleration
-        System.out.println("What is your acceleration?");
-        double acceleration;
-        acceleration = keyedInput.nextInt();
+        //force downwards
+        System.out.println("What is the magnitude of the force downward? (N)");
+        double forceDown;
+        forceDown = keyedInput.nextInt();
         
-        //Force
-        double force;
-        force = mass * acceleration; 
-        System.out.println("Your Force is: "+force+"N");
+        //Net Force
+        double forceNet;
+        forceNet = forceUp-forceDown; 
+        System.out.println("Your Net Force is: "+forceNet+"N [upwards]");
+}
+    
+    public static void acelerationOut() {
+        Scanner keyedInput = new Scanner(System.in);
+        
+        //force upwards
+        System.out.println("What is the magnitude of the force upward? (N)");
+        double forceUp;
+        forceUp = keyedInput.nextInt();
+        
+        //force downwards
+        System.out.println("What is the magnitude of the force downward? (N)");
+        double forceDown;
+        forceDown = keyedInput.nextInt();
+        
+        //Net Force
+        double forceNet;
+        forceNet = forceUp-forceDown; 
+        System.out.println("Your Net Force is: "+forceNet+"N [upwards]");
 }
     
 //////////////////////////////////////////////////////////////////////////
@@ -71,7 +90,7 @@ public class U2_A5_FirstSubroutine {
         System.out.println("-------------------------------------");
         System.out.println("What calculation would you like to attempt?\n");
         System.out.println("1)Find Force (know m+a)");
-        System.out.println("2)Find Net Force (know Fup+Fdown+Fleft+Fright)");
+        System.out.println("2)Find Net Force |Vertical| (know Fup+Fdown)");
         System.out.println("3)Find Acceleration (know v1+v2+t)");
         System.out.println("4)Find Gravitational Potential Energy (know m+h)");
         System.out.println("5)Average Grades\n");
